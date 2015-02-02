9774d86c9e5e06596764da477803a9571de7600c
Rob Thomas <xrobau@gmail.com>
2015-02-02 15:14:47 +1000

    ... Well, that seems to work

--- SQL/parse.sh
+++ SQL/parse.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sed -r 's/(ENGINE|DEFAULT.CHARSET|AUTO_INCREMENT)=\w+//gm' < newinstall.sql |  
-  sed -r 's/(PRIMARY KEY \(`modulename`,`featurename`\)),/\1/m' |
-  sed -r 's/(KEY `enabled` \(`enabled`\))/\/* \1 *\//m' |
+sed -r 's/(ENGINE|DEFAULT.CHARSET|AUTO_INCREMENT)=\w+//gm' < newinstall.sql |
+  sed -r 's/(PRIMARY KEY \(.+\)),/\1/m' |
+  sed -r 's/(KEY `enabled` \(`enabled`\))/\/* \1 *\//m' | # remove from featurecodes
+  sed -r 's/(KEY `time` \(`time`,`level`\))/\/* \1 *\//m' | # remove from freepbx_log
   sed -r 's/AUTO_INCREMENT/AUTOINCREMENT/m' |
-  set -r 's/int\(11\) NOT NULL AUTOINCREMENT/INTEGER PRIMARY KEY AUTOINCREMENT/m' |
+  sed -r 's/int\(11\) NOT NULL AUTOINCREMENT/INTEGER/m' |
+  sed -r 's/`level` enum\(.+\)/`level` CHAR(20)/m' |
   grep -v LOCK\ TAB > newinstall.sqlite
   
 sqlite3 /var/www/freepbx.db < newinstall.sqlite
